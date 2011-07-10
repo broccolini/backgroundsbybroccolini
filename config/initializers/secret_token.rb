ae4068b9931ccb7bf2b8b554b96a4b52b742a8b0
Shane Becker <veganstraightedge@gmail.com>
2011-07-09 18:16:39 -0700

    vanilla rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Backgroundsbybroccolini::Application.config.secret_token = '3590b4c425ef969fc4a840d115099d9c49bc90a766a464f25f133d88f1992053a136146fe17d23d1291a14bbd596c6cad3a903aa9f579377f5fbd0b5fc777ff7'

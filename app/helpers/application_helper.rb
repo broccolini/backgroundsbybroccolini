8f884a47624b633c65380e5c9c4fa810838b188e
Shane Becker <veganstraightedge@gmail.com>
2011-07-10 13:06:59 -0700

    fixed brand name in download link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   def link_to_download(background)
     slug  = background.name.downcase.gsub(/ |-/, "_")
-    brand = background.brand.name
-    url   = "https://github.com/broccolini/background_downloads/blob/master/downloads/#{brand}/#{slug}.png"
+    brand = background.brand.name.downcase.gsub(/ |-/, "_")
+    url   = "https://github.com/broccolini/background_downloads/raw/master/downloads/#{brand}/#{slug}.png"
     link_to "Download", url
   end
 end

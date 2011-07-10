e84be207d4b458e5087fa336a501b744b8d3e479
Shane Becker <veganstraightedge@gmail.com>
2011-07-10 14:00:49 -0700

    route shuffling

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Backgroundsbybroccolini::Application.routes.draw do
   root :to => "backgrounds#index"
 
-  resources :backgrounds, :except => [:show]
   resources :brands,      :except => [:show]
+  resources :backgrounds, :except => [:show]
 
-  match "/backgrounds/:slug" => "backgrounds#show", :as => :background
-  match "/brands/:slug"      => "brands#show",      :as => :brand
+  match "/brands/:slug"             => "brands#show",      :as => :brand
+  match "/backgrounds/:slug"        => "backgrounds#show", :as => :background
 
   match "/auth/:provider/callback" => "sessions#create"
   match "/signout"                 => "sessions#destroy", :as => :signout

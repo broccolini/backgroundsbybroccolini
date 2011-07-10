e87f6120ce1bdbe042cf51f03ffd34dd0c4e7769
Shane Becker <veganstraightedge@gmail.com>
2011-07-10 14:17:18 -0700

    uniq the name of brand

--- app/models/background.rb
+++ app/models/background.rb
@@ -1,6 +1,6 @@
 class Background < ActiveRecord::Base
   belongs_to :brand
 
-  validates :name,  :presence => true
+  validates :name,  :presence => true, :uniqueness => true
   validates :brand, :presence => true
 end

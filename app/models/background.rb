02f973ca90ef00d2b6b32074249639a0df408827
Shane Becker <veganstraightedge@gmail.com>
2011-07-10 00:46:28 -0700

    validations

--- app/models/background.rb
+++ app/models/background.rb
@@ -1,3 +1,6 @@
 class Background < ActiveRecord::Base
   belongs_to :brand
+
+  validates :name,  :presence => true
+  validates :brand, :presence => true
 end
--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,3 +1,6 @@
 class Brand < ActiveRecord::Base
   has_many :backgrounds
+
+  validates :name, :presence => true
+  validates :url,  :presence => { :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix }
 end

49cda99aed5816e5d8dbf452d7837fd30f79ec8c
Shane Becker <veganstraightedge@gmail.com>
2011-07-10 01:32:28 -0700

    cast user uid to string for pg in production

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
     login = request.env["omniauth.auth"]["extra"]["user_hash"]["login"]
 
     if %w(broccolini).include?(login)
-      user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+      user = User.find_by_provider_and_uid(auth["provider"], auth["uid"].to_s) || User.create_with_omniauth(auth)
       session[:user_id] = user.id
     end
     redirect_to root_url, :notice => "Signed in!"
